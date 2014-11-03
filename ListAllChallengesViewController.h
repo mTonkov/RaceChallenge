6122186fc6acc2f4142688ad3797958390d810fd
mTonkov <mtonkov@yahoo.com>
2014-11-03 11:01:29 +0200

    Initial commit
    Implemented login

---
+++ ListAllChallengesViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  RaceChallenge
+//
+//  Created by Admin on 11/2/14.
+//  Copyright (c) 2014 Telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface ListAllChallengesViewController : UIViewController
+
+@end
---
+++ ListAllChallengesViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  RaceChallenge
+//
+//  Created by Admin on 11/2/14.
+//  Copyright (c) 2014 Telerik. All rights reserved.
+//
+
+#import "ListAllChallengesViewController.h"
+
+@interface ListAllChallengesViewController ()
+
+@end
+
+@implementation ListAllChallengesViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  self.title = @"All Challenges";
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
