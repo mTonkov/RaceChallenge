0a7c70b9ecc9b3cb4476dc4f75114764a870e7b9
mTonkov <mtonkov@yahoo.com>
2014-11-08 18:58:28 +0200

    Who Challenged the current user Controller

---
+++ WhoChallengedMeViewController.h
@@ -0,0 +1,14 @@
+//
+//  WhoChallengedMeViewController.h
+//  RaceChallenge
+//
+//  Created by Admin on 11/8/14.
+//  Copyright (c) 2014 Telerik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WhoChallengedMeViewController
+    : UITableViewController <UIAlertViewDelegate>
+
+@end
--- WhoChallengedMeViewController.m
+++ WhoChallengedMeViewController.m
@@ -1,23 +1,22 @@
 //
-//  ViewController.m
+//  WhoChallengedMeViewController.m
 //  RaceChallenge
 //
-//  Created by Admin on 11/2/14.
+//  Created by Admin on 11/8/14.
 //  Copyright (c) 2014 Telerik. All rights reserved.
 //
 
-#import "ListAllChallengesViewController.h"
+#import "WhoChallengedMeViewController.h"
 #import "Challenge.h"
 #import <Parse/Parse.h>
-#import "DareOpponentViewController.h"
 #import "Reachability.h"
 
-@interface ListAllChallengesViewController ()
+@interface WhoChallengedMeViewController ()
 
 @end
 
-@implementation ListAllChallengesViewController {
-  NSMutableArray *_availableChallenges;
+@implementation WhoChallengedMeViewController {
+  NSMutableArray *_challengesToUser;
   PFUser *_currentUser;
   Reachability *networkReachability;
   NetworkStatus networkStatus;
@@ -27,44 +26,49 @@
   [super viewDidLoad];
   _currentUser = [PFUser currentUser];
   networkReachability = [Reachability reachabilityForInternetConnection];
-
+  [self initializeRefreshNotification];
   [self getChallengesFromBackend];
+  self.title = @"Who Challenged Me?";
 
-  self.refreshControl = [[UIRefreshControl alloc] init];
-  self.refreshControl.backgroundColor = [UIColor purpleColor];
-  self.refreshControl.tintColor = [UIColor whiteColor];
-  [self.refreshControl addTarget:self
-                          action:@selector(getChallengesFromBackend)
-                forControlEvents:UIControlEventValueChanged];
+  //    _downSwipeRecognizer =
+  //    [[UITapGestureRecognizer alloc] initWithTarget:self
+  //    action:@selector(swipe:)];
+  //    _downSwipeRecognizer.numberOfTapsRequired = 2;
+  //    [[UISwipeGestureRecognizer alloc] initWithTarget:self
+  //                                              action:@selector(swipe:)];
+  //    _downSwipeRecognizer.direction = UISwipeGestureRecognizerDirectionDown;
 
-  self.title = @"All Challenges";
+  //    [self.tableView addGestureRecognizer:_downSwipeRecognizer];
 }
 
-- (void)viewWillAppear:(BOOL)animated {
-
-  [[self tableView] reloadData];
+//- (IBAction)swipe:(UISwipeGestureRecognizer *)recognizer {
+//
+//  if (self.refreshControl) {
+//    return;
+//  }
+//  [self initializeRefreshNotification];
+//    [self.view removeGestureRecognizer:_downSwipeRecognizer];
+//    _downSwipeRecognizer = nil;
+//}
+- (void)viewDidAppear:(BOOL)animated {
 }
-
-
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
 }
 
 - (void)getChallengesFromBackend {
   if ([self checkInternetConnection]) {
-
     PFQuery *query = [Challenge query];
-    [query whereKey:@"ownerId" notEqualTo:_currentUser.objectId];
-    [query whereKeyDoesNotExist:@"challengerName"];
+    [query whereKey:@"ownerId" equalTo:_currentUser.objectId];
+    [query whereKeyExists:@"challengerName"];
     __weak id weakSelf = self;
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects,
                                               NSError *error) {
         if (!error) {
-          _availableChallenges = [[NSMutableArray alloc] initWithArray:objects];
+          _challengesToUser = [[NSMutableArray alloc] initWithArray:objects];
           [[weakSelf tableView] reloadData];
         }
-        [self setMessageForQueriedData];
+        [self messageIfNoDataRetrieved];
 
         if (self.refreshControl) {
           [self setRefreshingMessage];
@@ -105,15 +109,29 @@
   self.refreshControl.attributedTitle = attributedTitle;
 }
 
+- (void)initializeRefreshNotification {
+  if (!self.refreshControl) {
+    UIRefreshControl *refresh = [[UIRefreshControl alloc] init];
+
+    [refresh addTarget:self
+                  action:@selector(getChallengesFromBackend)
+        forControlEvents:UIControlEventValueChanged];
+    refresh.backgroundColor = [UIColor purpleColor];
+    refresh.tintColor = [UIColor whiteColor];
+    self.refreshControl = refresh;
+    [self setRefreshingMessage];
+  }
+}
+
 - (NSInteger)tableView:(UITableView *)tableView
     numberOfRowsInSection:(NSInteger)section {
-  return _availableChallenges.count;
+
+  return _challengesToUser.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  NSString *cellIdentifier = @"cellForAllChallenges";
-
+  NSString *cellIdentifier = @"cellForChallengesToUser";
   UITableViewCell *cell =
       [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
   if (!cell) {
@@ -121,33 +139,24 @@
                                   reuseIdentifier:cellIdentifier];
   }
 
-  Challenge *chal = [_availableChallenges objectAtIndex:indexPath.row];
-    UILabel *raceType = (UILabel *)[cell viewWithTag:1];
-  [raceType setText:[NSString stringWithFormat:@"%@ Race",
-                                               [chal.type capitalizedString]]];
-  UILabel *raceAuthor = (UILabel *)[cell viewWithTag:2];
-  [raceAuthor setText:chal.ownerName];
-  UILabel *raceLocation = (UILabel *)[cell viewWithTag:3];
+  Challenge *chal = [_challengesToUser objectAtIndex:indexPath.row];
+  UILabel *raceType = (UILabel *)[cell viewWithTag:1];
+  UILabel *opponent = (UILabel *)[cell viewWithTag:2];
+  UILabel *opponentEmail = (UILabel *)[cell viewWithTag:3];
+  UILabel *car = (UILabel *)[cell viewWithTag:4];
+  UILabel *raceLocation = (UILabel *)[cell viewWithTag:5];
+
+  [raceType setText:chal.type];
+  [opponent setText:chal.challengerName];
+  [opponentEmail setText:chal.challengerEmail];
+  [car setText:chal.challengerCar];
   [raceLocation setText:chal.location];
 
   return cell;
 }
 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-
-  if ([segue.identifier isEqualToString:@"dareOpponentSegue"]) {
-    DareOpponentViewController *destination = [segue destinationViewController];
-    NSIndexPath *ip = [self.tableView indexPathForSelectedRow];
-
-    [destination
-        setSelectedChallenge:[_availableChallenges objectAtIndex:ip.row]];
-    [destination setRetrievedChallenges:_availableChallenges];
-  }
-}
-
-#pragma mark Customizing table view
-- (void)setMessageForQueriedData {
-  if (_availableChallenges) {
+- (void)messageIfNoDataRetrieved {
+  if (_challengesToUser) {
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
     self.tableView.backgroundView = nil;
   } else {
@@ -156,10 +165,10 @@
                                  self.view.bounds.size.height)];
 
     messageLabel.text =
-        @"No data is currently available.\n Please, pull down " @"to refresh.";
+        @"No data is currently available.\n Please, pull down to refresh";
     messageLabel.textColor = [UIColor blackColor];
-    //        messageLabel.backgroundColor = [UIColor colorWithRed:1.0 green:0.6
-    //        blue:0.1 alpha:0.8];
+    //        messageLabel.backgroundColor = [UIColor colorWithRed:1.0
+    //        green:0.6        blue:0.1 alpha:0.8];
     messageLabel.numberOfLines = 0;
     messageLabel.textAlignment = NSTextAlignmentCenter;
     messageLabel.font = [UIFont fontWithName:@"Palatino-Italic" size:20];
@@ -177,5 +186,4 @@
                     cancelButtonTitle:@"OK"
                     otherButtonTitles:nil, nil] show];
 }
-
 @end
